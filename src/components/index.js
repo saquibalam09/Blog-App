35b352a5e9e95caa69f523def6590b047c4e99ca
saquib <saquibm860@gamil.com>
2024-03-22 16:07:57 +0530

    index file is updated

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import Container from './container/container';
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button';
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Button
 }
