45c248082dba8e1c6377bbe97eca3136698f53fc
saquib <saquibm860@gamil.com>
2024-03-22 14:17:39 +0530

    new files are being added and updated

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -28,7 +28,7 @@ export class Service{
             )
             
         } catch (error) {
-            console.log("Appwrite service :: logout :: error", error);
+            console.log("Appwrite service :: createPost :: error", error);
         }
 
     }
@@ -47,7 +47,7 @@ export class Service{
             )
             
         } catch (error) {
-            console.log("Appwrite service :: logout :: error", error);
+            console.log("Appwrite service :: updatePost :: error", error);
         }
     }
     async deletePost(slug){
@@ -59,7 +59,7 @@ export class Service{
             )
             return true;
         } catch (error) {
-            console.log("Appwrite service :: logout :: error", error);
+            console.log("Appwrite service :: deletePost :: error", error);
             return false;
 
         }
@@ -73,8 +73,55 @@ export class Service{
             )
             
         } catch (error) {
-            console.log("Appwrite service :: logout :: error", error);
+            console.log("Appwrite service :: getPost :: error", error);
         }
+    }
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false;        
+        }
+    }
+    // file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite serive :: uploadFile :: error", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite serive :: deleteFile :: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
     }
 }
 
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,4 +6,4 @@ function Logo({width = '100px'}) {
     )
 }
 
-export default Logo
+export default Logo;
